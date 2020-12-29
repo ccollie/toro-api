edee6d3647f2aac355f881f02b84ce013dd5227f
clayton collie <clayton.collie@turner.com>
2020-12-29 12:48:21 -0500

    feature(graphQl): Added host.ping, queue.ruleAlertCount, queue.ruleAlerts

---
+++ src/server/graphql/types/queue/model/Queue.ruleAlertCount.ts
@@ -0,0 +1,12 @@
+import { Queue } from 'bullmq';
+import { FieldConfig } from '../../utils';
+
+export const ruleAlertCount: FieldConfig = {
+  type: 'Int!',
+  description: 'Returns the count of rule alerts associated with a Queue',
+  args: {},
+  async resolve(queue: Queue, _, { supervisor }): Promise<number> {
+    const manager = supervisor.getQueueManager(queue);
+    return manager.getAlertCount();
+  },
+};
