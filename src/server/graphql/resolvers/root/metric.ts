a6f4212e2ec9ecde3b8c4ce469ac57316ab1d42b
clayton collie <clayton.collie@turner.com>
2021-07-03 14:30:21 -0400

    fix(metric): fix return value of Metric root query

--- src/server/graphql/resolvers/root/metric.ts
+++ src/server/graphql/resolvers/root/metric.ts
@@ -1,7 +1,6 @@
 import { FieldConfig } from '../index';
 import { getQueueManager } from '../../helpers';
 import { MetricTC } from '../metric/model';
-import { SerializedMetric } from '@src/types';
 import boom from '@hapi/boom';
 
 export const metric: FieldConfig = {
@@ -11,14 +10,12 @@ export const metric: FieldConfig = {
     queueId: 'ID!',
     metricId: 'ID!',
   },
-  async resolve(_, { queueId, metricId }): Promise<SerializedMetric> {
+  async resolve(_, { queueId, metricId }) {
     const manager = getQueueManager(queueId);
     const metric = await manager.metricManager.getMetric(metricId);
     if (!metric) {
       throw boom.notFound(`Cannot find a metric with the id "${metric.id}"`);
     }
-    const result = metric.toJSON();
-    (result as any).queueId = queueId;
-    return result;
+    return metric;
   },
 };
